aeddcc6da68106fd4b83b61685d329c1d8df15ad
Aryanka3801 <126391673+Aryanka3801@users.noreply.github.com>
2026-05-09 14:07:46 +0530

    Add TV file: app/src/main/java/com/example/myapp/MainFragment.java

---
+++ app/src/main/java/com/example/myapp/MainFragment.java
@@ -0,0 +1,21 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import androidx.leanback.app.BrowseSupportFragment;
+import androidx.leanback.widget.ArrayObjectAdapter;
+import androidx.leanback.widget.HeaderItem;
+import androidx.leanback.widget.ImageCardView;
+import androidx.leanback.widget.ListRow;
+import androidx.leanback.widget.ListRowPresenter;
+import androidx.leanback.widget.Presenter;
+import android.view.ViewGroup;
+
+public class MainFragment extends android.app.Fragment {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
