016dbe54678a664ad1d365a301463534bc683b77
Aryanka3801 <126391673+Aryanka3801@users.noreply.github.com>
2026-05-09 14:07:45 +0530

    Convert to Android TV: app/src/main/java/com/example/myapp/MainActivity.java

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.myapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_browse_fragment, new MainFragment())
+                .commitAllowingStateLoss();
+        }
     }
 }
